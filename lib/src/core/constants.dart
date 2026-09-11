d6c905e0dfe6137f97862801e7071297ab461833
Mahmoud Qussai <mahmoudqussai96@gmail.com>
2026-09-11 12:25:22 +0300

    security: remove exposed credentials from source

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  static const String TMDB_API_KEY = "cc8c71b4267d4a60d039a01502cdb536";
-  static const String READ_ACCESS_TOKEN =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzhjNzFiNDI2N2Q0YTYwZDAzOWEwMTUwMmNkYjUzNiIsIm5iZiI6MTcyMTYzMzA5Ny44NTkyMjEsInN1YiI6IjY2OWUwODY5MTQ0ZmYwNGJiMjRjNTJjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-J2VAJlHbEUjSm8bK22HPm5tl4dTttwjRCKx-dW72mA';
+  static const String tmdbReadAccessToken = String.fromEnvironment(
+    'TMDB_READ_ACCESS_TOKEN',
+  );
   static const String baseUrl = 'https://api.themoviedb.org';
-  static const String imageBaseUrl = 'http://image.tmdb.org/t/p/w185';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w185';
 }
